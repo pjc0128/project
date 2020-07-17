abc342f30662dc98078430f8fc47d0c94514fa19
pjc0128 <57315725+pjc0128@users.noreply.github.com>
2020-07-17 10:01:50 +0900

    init

---
+++ database/migrations/2020_07_17_004331_create_articles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('title');
+            $table->string('url');
+            $table->timestamps();
+
+            $table->foreign('id')->references('id')->on('mail_contents');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
